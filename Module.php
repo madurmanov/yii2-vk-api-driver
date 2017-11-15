0241ab27dc8ae75f71b93ed887fabc38a57fd303
Mikhail Durmanov <madurmanov@gmail.com>
2017-11-16 02:44:54 +0300

    Update widget

--- Module.php
+++ Module.php
@@ -5,20 +5,31 @@ namespace madurmanov\VkApiDriver;
 use Yii;
 use yii\base\Module as BaseModule;
 use yii\base\InvalidConfigException;
+use madurmanov\VkApiDriver\Widget;
 
 class Module extends BaseModule
 {
   const API = 'https://api.vk.com/method/';
 
+  public $appId = 0;
   public $ownerId = 0;
   public $accessToken = '';
 
   public function init()
   {
+    if (!$this->appId)
+      throw new InvalidConfigException('`appId` is not set');
     if (!$this->ownerId)
       throw new InvalidConfigException('`ownerId` is not set');
     if (!$this->accessToken)
       throw new InvalidConfigException('`accessToken` is not set');
+  }
+
+  public function getVkTokenButton()
+  {
+    return Widget::widget([
+      'appId' => $this->appId
+    ]);
   }
 
   public function request($method, $params)
--- Widget.php
+++ Widget.php
@@ -6,26 +6,21 @@ use Yii;
 use yii\base\Widget as BaseWidget;
 use yii\base\InvalidConfigException;
 
-class VkTokenButton extends BaseWidget
+class Widget extends BaseWidget
 {
-  public $moduleName = '';
-  public $redirectUrl = '';
-  public $ownerId = '';
+  public $appId = 0;
 
   public function init()
   {
     parent::init();
-    if (!$this->moduleName)
-      throw new InvalidConfigException('`moduleName` is not set');
-    if (!$this->redirectUrl)
-      throw new InvalidConfigException('`redirectUrl` is not set');
-    $this->ownerId = Yii::$app->getModule($this->moduleName)->ownerId;
+    if (!$this->appId)
+      throw new InvalidConfigException('`appId` is not set');
   }
 
   public function run()
   {
     return $this->render('vk-token-button', [
-       'url' => "https://oauth.vk.com/authorize?client_id={$this->ownerId}&display=page&redirect_uri={$this->redirectUrl}&scope=wall&response_type=token"
+       'url' => "https://oauth.vk.com/authorize?client_id={$this->appId}&display=page&redirect_uri=https://oauth.vk.com/blank.html&scope=wall&response_type=token"
     ]);
   }
 }
