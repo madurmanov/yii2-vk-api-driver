45c5b0a1302d03b39194b699d5c2cf85f27c1620
Mikhail Durmanov <madurmanov@gmail.com>
2017-11-16 16:47:23 +0300

    Improve Module

--- Module.php
+++ Module.php
@@ -2,52 +2,41 @@
 
 namespace madurmanov\VkApiDriver;
 
-use Yii;
 use yii\base\Module as BaseModule;
 use yii\base\InvalidConfigException;
 use madurmanov\VkApiDriver\Widget;
 
 class Module extends BaseModule
 {
-  const API = 'https://api.vk.com/method/';
+  const API = 'https://api.vk.com';
 
   public $appId = 0;
   public $ownerId = 0;
+  public $ownerIsCommunity = false;
   public $accessToken = '';
 
-  public function init()
-  {
-    if (!$this->appId)
-      throw new InvalidConfigException('`appId` is not set');
-    if (!$this->ownerId)
-      throw new InvalidConfigException('`ownerId` is not set');
-    if (!$this->accessToken)
-      throw new InvalidConfigException('`accessToken` is not set');
-  }
-
-  public function getVkTokenButton()
+  public function getTokenButton()
   {
     return Widget::widget([
       'appId' => $this->appId
     ]);
   }
 
-  public function request($method, $params)
+  public function request($method, $params, $accessToken = false)
   {
-    $query = http_build_query($params);
-    $url = self::API . "$method?$query";
+    if ($accessToken) $params['access_token'] = $this->accessToken;
+    $url = self::API . "/method/{$method}?" . http_build_query($params);
     return json_decode(file_get_contents($url));
   }
 
-  public function wallPost($message = '', $attachments = '')
+  public function wallPost($fromGroup = 1, $message = '', $attachments = '')
   {
     $params = [
-      'owner_id' => "-{$this->ownerId}",
-      'access_token' => $this->accessToken,
-      'from_group' => 1
+      'owner_id' => ($this->ownerIsCommunity ? '-' : '') . $this->ownerId,
+      'from_group' => $fromGroup
     ];
     if ($message) $params['message'] = $message;
     if ($attachments) $params['attachments'] = $attachments;
-    return $this->request('wall.post', $params);
+    return $this->request('wall.post', $params, true);
   }
 }
