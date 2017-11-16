97aa91e7605a69cd59f386de5cbbccca47459b85
Mikhail Durmanov <madurmanov@gmail.com>
2017-11-16 16:48:24 +0300

    Fix bug in getting access token from production server

--- Widget.php
+++ Widget.php
@@ -2,7 +2,6 @@
 
 namespace madurmanov\VkApiDriver;
 
-use Yii;
 use yii\base\Widget as BaseWidget;
 use yii\base\InvalidConfigException;
 
@@ -10,17 +9,10 @@ class Widget extends BaseWidget
 {
   public $appId = 0;
 
-  public function init()
-  {
-    parent::init();
-    if (!$this->appId)
-      throw new InvalidConfigException('`appId` is not set');
-  }
-
   public function run()
   {
     return $this->render('vk-token-button', [
-       'url' => "https://oauth.vk.com/authorize?client_id={$this->appId}&display=page&redirect_uri=https://oauth.vk.com/blank.html&scope=wall&response_type=token"
+       'url' => "https://oauth.vk.com/authorize?client_id={$this->appId}&display=page&redirect_uri=https://oauth.vk.com/blank.html&scope=wall,offline&response_type=token"
     ]);
   }
 }
